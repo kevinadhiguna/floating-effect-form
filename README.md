f77433f65deedce0114e7252939148d0bb248817
kevinadhiguna <kevin26798@gmail.com>
2020-08-10 16:00:24 +0700

    build: version 0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Floating Effect Form
+
+Floating Effect Form is a login form with placeholders with floating effect on each input column. 
---
+++ js/main.js
@@ -0,0 +1,12 @@
+let input_fields = document.querySelectorAll('input');
+
+for (let i = 0; i < input_fields; i++) {
+    const field = input_fields[i];
+    field.addEventListener('input', function (e) {
+        if (e.target.value != "") {
+            e.target.parentNode.classList.add('has-content');
+        } else if (e.target.value == "") {
+            e.target.parentNode.classList.remove('has-content');
+        }
+    });
+}
